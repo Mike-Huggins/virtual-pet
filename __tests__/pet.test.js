9667c733fd04ccecbcdd5af7d4bb9d66b5a6fe12
Mike H <mikeyh1001@hotmail.com>
2019-02-22 11:18:55 +0000

    age increment added with test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,13 +1,25 @@
 const { Pet } = require('../src/pet.js');
 
 describe('Pet', () => {
-    xit('creates a new pet when parameters are passed into the constructor function', () => {
+    it('creates a new pet when parameters are passed into the constructor function', () => {
       expect(new Pet()).toBeInstanceOf(Object)
     });
 
-    xit('creates a new pet when parameters are passed into the constructor function', () => {
+    it('creates a new pet when parameters are passed into the constructor function', () => {
         const pet = new Pet ('PoochyPoo');
         expect(pet.name).toEqual('PoochyPoo')
       });
+    
+    it ('Test that the pet age is 0 at the start', () => {
+        const pet = new Pet ('PoochyPoo');
+        expect (pet.age).toEqual(0)
+    });
+
+    it ('Check that the growUp method works correctly and iterates the age by 1', () => {
+        const pet = new Pet ('PoochyPoo');
+        expect (pet.age).toEqual(0)
+        pet.growUp();
+        expect (pet.age).toEqual(1)
+    });
 
 });
