58692007f03349b70bf945711de4082a1e6e4ad5
Mike H <mikeyh1001@hotmail.com>
2019-02-22 10:53:55 +0000

    pet name test and constructor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,11 +2,12 @@ const { Pet } = require('../src/pet.js');
 
 describe('Pet', () => {
     xit('creates a new pet when parameters are passed into the constructor function', () => {
-      expect(new Pet('Bowwow')).toBeInstanceOf(Object)
+      expect(new Pet()).toBeInstanceOf(Object)
     });
 
     xit('creates a new pet when parameters are passed into the constructor function', () => {
-        expect(new Pet ('Poochypoo')).toBeInstanceOf(Object)
+        const pet = new Pet ('PoochyPoo');
+        expect(pet.name).toEqual('PoochyPoo')
       });
 
 });
