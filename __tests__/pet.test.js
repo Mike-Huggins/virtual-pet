ce07744f74cd749e69cd0efb6b85900d4f64bc89
Mike H <mikeyh1001@hotmail.com>
2019-02-22 13:31:56 +0000

    walk function added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,28 +1,28 @@
 const { Pet } = require('../src/pet.js');
 
 describe('Pet', () => {
-    it('creates a new pet when parameters are passed into the constructor function', () => {
+    xit('creates a new pet when parameters are passed into the constructor function', () => {
       expect(new Pet()).toBeInstanceOf(Object);
     });
 
-    it('creates a new pet when parameters are passed into the constructor function', () => {
+    xit('creates a new pet when parameters are passed into the constructor function', () => {
         const pet = new Pet ('PoochyPoo');
         expect(pet.name).toEqual('PoochyPoo');
       });
     
-    it ('Test that the pet age is 0 at the start', () => {
+    xit ('Test that the pet age is 0 at the start', () => {
         const pet = new Pet ('PoochyPoo');
         expect(pet.age).toEqual(0);
     });
 
-    it ('Check that the growUp method works correctly and iterates the age by 1', () => {
+    xit ('Check that the growUp method works correctly and iterates the age by 1', () => {
         const pet = new Pet ('PoochyPoo');
         expect(pet.age).toEqual(0);
         pet.growUp();
         expect(pet.age).toEqual(1);
     });
 
-    it ('check that growUp correctly implements new hunger and fitness levels', () => {
+    xit ('check that growUp correctly implements new hunger and fitness levels', () => {
       const pet = new Pet ('PoochyPoo');
       expect(pet.hunger).toEqual(0);
       expect(pet.fitness).toEqual(10);
@@ -30,6 +30,24 @@ describe('Pet', () => {
       expect(pet.hunger).toEqual(5);
       expect(pet.fitness).toEqual(7);
 
+    });
+
+    it ('Test walk function effect on fitness', () => {
+      const pet = new Pet ('PoochyPoo');
+      expect(pet.hunger).toEqual(0);
+      expect(pet.fitness).toEqual(10);
+      pet.growUp();
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(7);
+      pet.growUp();
+      expect(pet.hunger).toEqual(10);
+      expect(pet.fitness).toEqual(4);
+      pet.walk();
+      expect(pet.fitness).toEqual(8);
+      pet.walk();
+      expect(pet.fitness).toEqual(10);
+      pet.walk();
+      expect(pet.fitness).toEqual(10);
     });
 
 });
--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,26 @@
+const maximumFitness = 10;
+const ageAtBirth = 0
+
 function Pet(name) {
     this.name = name;
-    this.age = 0;
+    this.age = ageAtBirth;
     this.hunger = 0;
-    this.fitness = 10
+    this.fitness = maximumFitness;
   }
 
 Pet.prototype.growUp = function() { 
   this.age ++; 
   this.hunger +=5;
   this.fitness -=3;
+}
+
+Pet.prototype.walk = function() {
+  if(this.fitness >= 6){
+  this.fitness = maximumFitness
+  };
+  if(this.fitness < 6){
+    this.fitness +=4
+  };
 }
 
 
